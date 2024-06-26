8e2da104efc4f367dfce6b37ccadcd3ad847d726
sidaswal7 <siddhantaswal7@gmail.com>
2024-06-26 10:53:33 +0530

    used links to navigate

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,33 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
+const details = [
+
+    { id : 1, name: 'Yash', role: 'Senior Developer'},
+    
+    { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
+    
+    { id : 3, name: 'Suresh', role: 'Frontend Developer'}
+    
+    ];
+    
+
 const AboutUsPage = ()=>{
     return(
-        <h1>This is AboutUs Page</h1>
+        <Fragment>
+            <h1>This is AboutUs Page</h1>
+            <ul>
+                {details.map((dev)=>{
+                    return(
+                    <li key={dev.id}>
+                        <Link href={`/about/${String(dev.id)}`}>{dev.name}</Link>
+                    </li>
+                    
+                )})}
+            </ul>
+        </Fragment>
+
+
     )
 
 }
